0d205774f54f6614a9e421afbc45fa22bba8b213
Brandon Mathis <bemathis@gmail.com>
2026-09-14 16:18:06 -0400

    Cleanup junk

--- app/controllers/api/hello_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Api
-  class HelloController < ActionController::API
-    def show
-      render json: { hello: 'World' }
-    end
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,10 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   namespace :api do
-    get 'hello', to: 'hello#show'
     get 'roster', to: 'roster#index'
     post 'imports/preview', to: 'imports#preview'
     post 'imports/apply', to: 'imports#apply'
   end
-
-  get 'hello', to: 'api/hello#show', constraints: lambda { |request|
-    !request.format.html?
-  }
 
   # Defines the root path route ("/")
   root 'spa#index'
